3dba4f59c358fde4365ff71a5b562ab87e05ce3b
Rishueee <134083360+Rishueee@users.noreply.github.com>
2023-11-24 11:40:46 +0530

    Add files via upload

---
+++ src/components/Image.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Image = (props) => {
+    const imageData = props.imageData;
+  return (
+    <div>
+      {
+        imageData.map(image=>{
+          return(
+            <div key={image.id} className="column">
+              <img src={image.img} alt=""></img>
+            </div>
+          )
+        })
+      }
+    </div>
+  )
+}
+
+export default Image
